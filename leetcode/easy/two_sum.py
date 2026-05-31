c8e2894b130c798732e71adabba868c7a487f021
i-chernousov <iche7n@gmail.com>
2026-05-31 13:34:19 +0300

    Create two_sum.py

---
+++ leetcode/easy/two_sum.py
@@ -0,0 +1,46 @@
+"""
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+"""
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    """ Найти индексы  элементов, сумма которых равна target. """
+
+    i = 0
+
+    while i < len(nums):
+        j = i + 1
+
+        while j < len(nums):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+            j += 1
+
+        i += 1
+
+    return []
+
+if __name__ == "__main__":
+    print(two_sum([3, 2, 3], 6))
