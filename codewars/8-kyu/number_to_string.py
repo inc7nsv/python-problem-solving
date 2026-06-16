f18f4c8f7033f3d31ccd5b5b91558fdc418a1c5a
i-chernousov <iche7n@gmail.com>
2026-06-16 22:36:32 +0300

    Create number_to_string.py

---
+++ codewars/8-kyu/number_to_string.py
@@ -0,0 +1,12 @@
+"""
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?
+"""
+
+
+def number_to_string(num: int):
+    """ Функция преобразует целочисленный тип данных в строку"""
+    return str(num)
+
+if __name__ == "__main__":
+    print(number_to_string(123))
