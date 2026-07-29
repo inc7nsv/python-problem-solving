16468019fbcbd2bd10c59fbfdffe1a02c059f304
i-chernousov <iche7n@gmail.com>
2026-07-29 22:46:36 +0300

    Create double_integer.py

---
+++ codewars/8-kyu/double_integer.py
@@ -0,0 +1,13 @@
+"""
+Code as fast as you can! Y
+
+ou need to double the integer and return it.
+"""
+
+
+def double_integer(number: int) -> int:
+    """ Функция удваивает целое число и возвращает результат """
+    return number + number
+
+if __name__ == "__main__":
+    print(double_integer(-10))
